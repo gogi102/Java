a156484564c00cc86f1c4d7485dd75afc3bcd884
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-10-16 18:18:15 +0900

    Create 블랙박스class.java

---
+++ 블랙박스class.java
@@ -0,0 +1,30 @@
+package blackbox;
+
+public class _01_class {
+    public static void main(String[] args) {
+        //객체 지향 프로그래밍(00P:object-oriented programming)
+        //유지보수 용이
+        //재사용성이 높음
+
+        //차량용 블랙박스
+        //모델명, 해상도, 가격, 색상
+
+        //첫 번째 제품
+        String modelname = "까망이";
+        String resolution = "FHD";
+        int price = 200000;
+        String color = "black";
+
+        //새로운 제품을 개발한다면?
+        String modelname2 = "하양이";
+        String resolution2 = "UHD";
+        int price2 = 300000;
+        String color2 = "white";
+
+        //또 새로운 제품을 개발한다면?
+        //이렇게 50개의 제품을 개발했다면?
+
+
+
+    }
+}
