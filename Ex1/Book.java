384867430811a455a6ae9ccb81f103c63735df82
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2024-05-08 18:08:11 +0900

    Add files via upload

---
+++ Ex1/Book.java
@@ -0,0 +1,19 @@
+package Ex1;
+
+public class Book extends Item{
+
+    public String author;
+    public String isbn;
+
+    public Book(String name, int price,String author, String isbn){
+        super(name,price);
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    @Override
+    public void print(){
+        super.print();
+        System.out.println("저자: "+ author+", isbn: "+isbn );
+    }
+}
